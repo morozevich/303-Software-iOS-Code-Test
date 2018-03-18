1d7e1b4d80174963e23085c9bec274cfc228e54a
Carlos <aquinoqcd29@gmail.com>
2018-03-18 20:55:49 -0300

    Initial class for uitableviewcell

---
+++ Data303/View/UserTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  UserTableViewCell.swift
+//  Data303
+//
+//  Created by carlos on 18/3/18.
+//  Copyright © 2018 carlos. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+    
+}
